c0c4163dff34ad2bffe1cce4fb0085fd2af34928
nikhil9987 <73032701+nikhil9987@users.noreply.github.com>
2024-10-09 12:34:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+st.title("News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_index"
+
+main_placeholder = st.empty()
+llm = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.6)
+embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    vectorstore_geminiai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a pickle file
+    vectorstore_geminiai.save_local("faiss_index")
+
+query = main_placeholder.text_input("Question: ")
+
+if query:
+    if os.path.exists(file_path):
+            vectorstore = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization = True)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
